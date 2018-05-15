094392b995c3da1b33ad7e8c37a5ed718fc43d97
João Manoel Neto <joaaomanooel@gmail.com>
2018-05-15 19:45:05 -0300

    Smock test

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,33 +1,30 @@
-describe('Main', () => {
-  let arr;
-  // Roda antes de todos os blocos
-  before(() => {
+const expect = require('chai').expect;
+const calc = require('../src/main');
 
-  });
-  // Roda depois de todos os blocos
-  after(() => {
+describe('Calc', () => {
+  describe('Smoke tests', () => {
+    it('should exist the calc lib', () => {
+      expect(calc).to.exist;
+    });
 
-  });
-  // Roda antes de cada bloco
-  beforeEach(() => {
-    arr = [1, 2, 3];
-  });
-  // Roda depois de cada bloco
-  afterEach(() => {
+    it('should exist the method sum', () => {
+      expect(calc.sum).to.exist;
+      expect(calc.sum).to.be.a('function');
+    });
 
-  });
+    it('should exist the method sub', () => {
+      expect(calc.sub).to.exist;
+      expect(calc.sub).to.be.a('function');
+    });
 
-  it('Should have a size of 4 when push another value to the array', () => {
-    arr.push(4);
-    console.log(arr.length);
-  });
+    it('should exist the method mult', () => {
+      expect(calc.mult).to.exist;
+      expect(calc.mult).to.be.a('function');
+    });
 
-  it('Should have a size of 2 when pop another value from the array', () => {
-    arr.pop();
-    console.log(arr.length);
-  });
-
-  it('Should remove the value 2 when use pop in the array', () => {
-    console.log(arr.pop() === 3);
+    it('should exist the method div', () => {
+      expect(calc.div).to.exist;
+      expect(calc.div).to.be.a('function');
+    });
   });
 });
